fd883464c31aa97fab1f792e38edb737a0004536
javieras79 <javieras79@gmail.com>
2019-01-02 11:16:12 +0100

    primera subida

---
+++ cabecera.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+	<meta charset="utf-8">
+	<title>Armys JavierAznar</title>
+	<meta name="description" content="Tienda online de airsoft">
+	<link href="css/bootstrap.css" rel="stylesheet"/>
+	<script src="js/validaciones.js"></script>
+	<!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link rel="shortcut icon" href="ico/favicon.ico">
+</head>
+
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+    session_start();
+    include("cabecera.php");
+    include("menu.php");
+?>
+<!--  En este área incluyo las catergorias  -->
+<?php
+    include("categorias.php");
+?>
+<?php 
+    include("pie.php");
+?>
